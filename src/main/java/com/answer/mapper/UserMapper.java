b87298b69f79c4c5f06450c78f1761cbbf34e2f4
Amentcity <1954869697@qq.com>
2025-01-29 00:17:37 +0800

    add 项目初始化

---
+++ src/main/java/com/answer/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.answer.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.answer.model.entity.User;
+
+/**
+ * 用户数据库操作
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
